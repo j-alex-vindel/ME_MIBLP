9c31b082d9e3bad1677b4c58340a8bd7cec0d4ac
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-01-30 17:42:50 +0000

    Yeast (inner eq) experiments updated

--- B_Yeast/run_experiment_k1.py
+++ B_Yeast/run_experiment_k1.py
@@ -3,18 +3,25 @@ import os
 
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'MY_MODULES'))) 
 
-from Ob_Met_Net_solmethods import MILP_solve_NOP,CB_sol_2_NOP, Inner_check_vs_ys_NOP
+from Ob_Met_Net_solmethods import MILP_solve_NOP,CB_sol_2_NOP, Inner_check_vs_ys_NOP,Result,Result_cb
 from Ob_Met_Net_Yeast import MetNet_Yeast
 
 import pandas as pd
 
 metnet = MetNet_Yeast
+ks = int(sys.argv[0][-4])
 
-# =========================================== K=1 ===========================
+# =========================================== K=2 ===========================
+try:
+    m = MILP_solve_NOP(network=metnet,k=2,log=False,speed=True)
+except:
+    m = Result(metnet.Name,['NA' for i in range(ks)],[1 for i in metnet.M],metnet.FBA,'inf','NA') 
 
-m = MILP_solve_NOP(network=metnet,k=1,log=False)
-cb = CB_sol_2_NOP(network=metnet,k=1,log=False)
-
+try:
+    cb = CB_sol_2_NOP(network=metnet,k=2,log=False,speed=True)
+except:
+    cb = Result_cb(metnet.Name,['NA'for i in range(ks)],[1 for i in metnet.M],metnet.FBA,metnet.FBA,'inf','NA')
+    
 r  = {
             'Method':['MILP','CB'],
             'Biomass':[m.Vs[metnet.biomass],cb.Vs[metnet.biomass]],
@@ -23,7 +30,7 @@ r  = {
             'Strategy':[m.Strategy,cb.Strategy]
 }
 
-file_name_r = f"../Results/Methods_K{sys.argv[0][-4]}_{metnet.Name}.csv"
+file_name_r = f"../Results/EQ/Methods_K{sys.argv[0][-4]}_{metnet.Name}.csv"
 
 isfile_r = os.path.exists(file_name_r)
 
@@ -58,11 +65,12 @@ r_inner = {'From':      ['CB'            ,'MILP'         ,'CB'            ,'MILP
            'V_biomass': [cb_vs_b.Biomass ,m_vs_b.Biomass ,cb_ys_b.Biomass ,m_ys_b.Biomass ,cb_vs_c.Biomass ,m_vs_c.Biomass ,cb_ys_c.Biomass ,m_ys_c.Biomass],
            'V_chemical':[cb_vs_b.Chemical,m_vs_b.Chemical,cb_ys_b.Chemical,m_ys_b.Chemical,cb_vs_c.Chemical,m_vs_c.Chemical,cb_ys_c.Chemical,m_ys_c.Chemical],
            'Soltype':   [cb_vs_b.Soltype ,m_vs_b.Soltype ,cb_ys_b.Soltype ,m_ys_b.Soltype ,cb_vs_c.Soltype ,m_vs_c.Soltype ,cb_ys_c.Soltype ,m_ys_c.Soltype],
-           'Strategy':  [cb.Strategy     ,m.Strategy     ,cb.Strategy     ,m.Strategy     ,cb.Strategy     ,m.Strategy     ,cb.Strategy     ,m.Strategy    ]
+           'Strategy':  [cb.Strategy     ,m.Strategy     ,cb.Strategy     ,m.Strategy     ,cb.Strategy     ,m.Strategy     ,cb.Strategy     ,m.Strategy    ],
+             'K':  [len(cb.Strategy)     ,len(m.Strategy)     ,len(cb.Strategy)     ,len(m.Strategy)     ,len(cb.Strategy)     ,len(m.Strategy)     ,len(cb.Strategy)     ,len(m.Strategy)    ]
     }
 
 
-file_name_ri = f"../Results/IC_K{sys.argv[0][-4]}_{metnet.Name}.csv"
+file_name_ri = f"../Results/EQ/IC_K{sys.argv[0][-4]}_{metnet.Name}.csv"
 isfile_ri = os.path.exists(file_name_ri)
 
 if not isfile_ri:
