d57690b29cf50652d1c6db525e4b1eb58daae372
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-02-07 17:31:31 +0000

    kn experiments run

--- B_iJO1366/run_experiment_p_k1.py
+++ B_iJO1366/run_experiment_p_k1.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'MY_MODULES'))) 
 
-from Ob_Met_Net_solmethods import CB_sol_2_P
+from CB_Sol_P import CB_P
 from Ob_Met_Net_ijo1366 import MetNet_ijo1366
 
 import pandas as pd
@@ -11,10 +11,12 @@ import pandas as pd
 metnet = MetNet_ijo1366
 K = int(sys.argv[0][-4])
 
-cb = CB_sol_2_P(network=metnet,k=K,log=True)
+cb = CB_P(network=metnet,k=K,log=True)
 
 print(f"Solutions")
 
 print(f"VS biomas: {cb.Vs[metnet.biomass]}")
 print(f"VS chemical: {cb.Vs[metnet.chemical]}")
 print(f"Strategy:{cb.Strategy}")
+
+
