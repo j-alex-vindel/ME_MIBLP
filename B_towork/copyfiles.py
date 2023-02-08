1aede761fcd8c2e8c7950342d66f3c1f09a47444
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-02-08 10:59:40 +0000

    copyfiles modified

--- B_towork/copyfiles.py
+++ B_towork/copyfiles.py
@@ -1,75 +1,46 @@
 import sys
 import os
-import time
 import shutil
 
-
-def copyfile_name(names:list=None,t:int=None):
-    path = os.getcwd()
-    print(f"Current Directory <{path}>")
-
-    parent_directory = os.path.abspath(os.path.join(path,os.pardir))
-    print(f"Parent Directory <{parent_directory}>")
-
-    for name in names:
-        folder = f"B_{name}"
-        destination = os.path.join(parent_directory,folder)
-        sizedir = len(os.listdir(destination))
-        for k in [1,2,3]:
-            file = f"../Metabolic_Engineering_Bilevel/Templates/run_experiment_k{k}.py"
-            
-            source = os.path.abspath(os.path.join(parent_directory,file))
-
-            print(f">> Copying from <{source}>")
-            print(f">> Pasting to <{destination}>")
-            time.sleep(t)
-         
-            shutil.copy(source,destination)
-            
-            if sizedir < len(os.listdir(destination)):
-                
-                print('Successful')
-                print(os.listdir(destination))
-                time.sleep(t+1)
-    print(f'''
-        TERMINATED!!!!
-    
-    ''')
-
-
-def copy_file_chname(cfile:str=None,bacteria:list=None,nfiles:int=None,t:int=None):
-
+def file_copier(folder:str=None,o_file:str=None,d_folder:str=None,c_file:str=None):
     path = os.getcwd()
     parent_directory = os.path.abspath(os.path.join(path,os.pardir))
-    print(f"Parent Directory <{parent_directory}>")
-    sfile = f"Templates/{cfile}"
-    source = os.path.abspath(os.path.join(parent_directory,sfile))
-
-    folder = f"B_{bacteria}"
-    dest_folder = os.path.join(parent_directory,folder)
-    cursizedir = len(os.listdir(dest_folder))
-    print(dest_folder)
-
-    ks = [i+1 for i in range(nfiles)]
     
-    print(f'From {source} ->')
+    print(f">> Information about Source <<")
+    folder = input(">> Enter name of the folder: \n -> ")
+    original_folder = os.path.join(parent_directory,folder)
+    print(f"Files in the source folder : \n {os.listdir(original_folder)}")
+    
+    
+    
+    o_file = input(">> Enter the name of the file: \n -> ")
+    source = f"{folder}/{o_file}"
+    source_dir = os.path.abspath(os.path.join(parent_directory,source))
 
-  
-    file = f"run_experiment_p_kn.py"
-    dest_file = os.path.join(dest_folder,file)
-    print(dest_file)
-    shutil.copyfile(source,dest_file)
 
-    finsizedir = len(os.listdir(dest_folder))
 
-    print(f"# {finsizedir-cursizedir} Files Added!!")
+    print(f">> Information about Destination <<")
+    d_folder = input(">> Enter name of the folder: \n -> ") 
+    d_dir = os.path.join(parent_directory,d_folder)
+
+    print(f"Files in the destinarion folder : \n {os.listdir(d_dir)}")
+
+    c_file = input(">> Enter new name of file: \n -> ")
+    newfile = os.path.join(d_dir,c_file)
+
+    shutil.copyfile(source_dir,newfile)
+
+    print("New file copied!!")
+
 
 
 if __name__ == '__main__':
-    # copyfile_name(names=sys.argv[1:len(sys.argv)-1],t=int(sys.argv[-1]))
-    file_to_copy = 'run_experiment_p_kn.py'
-    bacterias = ['iAF1260','iJO1366','iJR904']
-    n_copies = 1
-    for bacteria in bacterias:
-        copy_file_chname(cfile=file_to_copy,bacteria=bacteria,nfiles=int(n_copies))
+    file_copier()
+
+
+
+
+
+
+
 
