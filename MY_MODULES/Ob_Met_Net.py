f61c49c33d81914d5f4adc449d519b503d1a4644
j-alex-vindel <jose.vindel-garduno@strath.ac.uk>
2023-02-21 18:12:36 +0000

    module updated

--- MY_MODULES/Ob_Met_Net.py
+++ MY_MODULES/Ob_Met_Net.py
@@ -13,6 +13,8 @@ Index = NewType('Index',int)
 Big_M = NewType('Big M', int)
 Target = NewType('Prod Target %',float)
 
+class FBAError(Exception):
+    pass
 
 class Metabolic_Network:
 
@@ -62,12 +64,11 @@ class Met_Net_2:
                  Met: Metabolites = None,
                  KO: Knockouts = None, 
                  Name: str = None, 
-                 Biomass: Index = None, 
-                 Chemical: Index = None, 
+                 biomass: Index = None, 
+                 chemical: Index = None, 
                  infeas:float = 1e-6, 
                  time_limit: int = 1000, 
-                 BM: Big_M = 1000,
-                 target:float=.5):
+                 BM: Big_M = 1000):
         
         self.S = S
         self.LB = LB
@@ -76,32 +77,38 @@ class Met_Net_2:
         self.Met = Met
         self.KO = KO
         self.Name = Name
-        self.Biomass = Biomass
-        self.Chemical = Chemical
+        self.biomass = biomass
+        self.chemical = chemical
         self.infeas = infeas
         self.time_limit = time_limit
         self.BM = BM
         self.M = set_constructor(self.Rxn)
         self.N = set_constructor(self.Met)
-        self.target = target
+        self.FBA = wildtype_FBA(self)
+
 
     @property
     def FBA(self):
         return self.FBA
+
     @property
     def minprod(self):
         return self.minprod
+
     @property
     def target(self):
         return self.target
+
     @FBA.setter
     def FBA(self):
-        self.FBA = wildtype_FBA(self)
+        raise FBAError('FBA information can only be accesed')
+
     @minprod.setter
     def minprod(self):
         self.minprod = self.target*self.FBA[self.biomass]
+
     @target.setter
-    def target(self,target):
+    def target(self,target=.5):
         self.target = target
 
     @property
@@ -110,9 +117,11 @@ class Met_Net_2:
     @property
     def c(self):
         return self.c
+
     @b.setter
     def b(self):
         self.b = np.array([0 for i in self.N])
+
     @c.setter
     def c(self):
-        self.c = np.array([1 if i == self.Biomass else 0 for i in self.M])
+        self.c = np.array([1 if i == self.biomass else 0 for i in self.M])
